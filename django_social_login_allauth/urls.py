46605270dce875c8e27bec96f10a3b3f16094a35
hellojunho <jo62698476@gmail.com>
2024-03-16 19:01:13 +0900

    refactor: 플랫폼 별 url path 수정

--- django_social_login_allauth/urls.py
+++ django_social_login_allauth/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import profile
+from .views import google_profile, kakao_profile, instagram_profile
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/profile/', profile, name="profile")
+    path('accounts/google/profile/', google_profile, name="google_profile"),
+    path('accounts/kakao/profile/', kakao_profile, name="kakao_profile"),
+    path('accounts/instagram/profile/', instagram_profile, name='instagram_profile'),
 ]
